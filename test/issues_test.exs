de485484b0cd76e1fd334c8f1947574c9a6df1ec
若帆 叶 <ruofan.ye@gu3.co.jp>
2017-09-07 18:59:10 +0900

    mix project first step

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -2,7 +2,7 @@ defmodule IssuesTest do
   use ExUnit.Case
   doctest Issues
 
-  test "greets the world" do
-    assert Issues.hello() == :world
+  test "first test" do
+    assert 1 + 1 == 2
   end
 end
