f34b076d0ac302fbca6c3903be8c29ab2f07e593
Ye Ruofan <kanaeye@YenoMacBook-Pro.local>
2017-09-07 23:54:23 +0900

    well tableformatter done

---
+++ _build/test/lib/issues/ebin/issues.app
@@ -0,0 +1,8 @@
+{application,issues,
+             [{applications,[kernel,stdlib,elixir,logger,httpoison,poison]},
+              {description,"issues"},
+              {modules,['Elixir.Issues','Elixir.Issues.CLI',
+                        'Elixir.Issues.GithubIssues']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {extra_applications,[logger,httpoison]}]}.
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,9 @@ defmodule CliTest do
     use ExUnit.Case 
     doctest Issues
 
-    import Issues.CLI, only: [parse_args: 1]
+    import Issues.CLI, only: [parse_args: 1,
+                              sort_into_ascending_order: 1,
+                              convert_to_list_of_maps: 1]
 
     test ":help returned by option parsing with -h and --help" do
         assert parse_args(["-h", "anything"]) == :help
@@ -16,4 +18,16 @@ defmodule CliTest do
     test "count si defauled if two values given" do
         assert parse_args(["user", "project"]) == {"user", "project", 4}
     end
+
+    test "sort test" do
+      result = sort_into_ascending_order(fake_created_list(["c", "a", "b"]))
+      issues = for issue <- result, do: issue["created_at"]
+      assert issues == ~w[a b c]
+    end 
+
+    defp fake_created_list(values) do
+      data = for value <- values,
+             do: [{ "created_at", value}, {"other_data", "xxx"}]
+      convert_to_list_of_maps data
+    end 
 end
